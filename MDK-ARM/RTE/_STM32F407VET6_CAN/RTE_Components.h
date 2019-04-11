f4d5a7fe471e046217798774ffe08962791eaef7
Zhang263410 <1546812989@qq.com>
2019-04-11 20:28:28 +0800

    first push is test include the can interface of stm32

---
+++ MDK-ARM/RTE/_STM32F407VET6_CAN/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32F407VET6_CAN' 
+ * Target:  'STM32F407VET6_CAN' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
